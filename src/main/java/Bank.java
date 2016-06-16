f661209db7cfa59e57d41e8ab98a4d422b53e8eb
rashid <rshaikh@equalexperts.com>
2016-06-16 15:27:36 +0530

    Step-3 Added Expression and Bank

---
+++ src/main/java/Bank.java
@@ -0,0 +1,8 @@
+/**
+ * Created by rashid on 16/6/16.
+ */
+public class Bank {
+    public Money reduce(Expression source, String to) {
+        return source.reduce(to);
+    }
+}
---
+++ src/main/java/Expression.java
@@ -0,0 +1,6 @@
+/**
+ * Created by rashid on 16/6/16.
+ */
+public interface Expression {
+    Money reduce(String to);
+}
