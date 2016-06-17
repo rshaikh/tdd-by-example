5fe15c3311b535cbe69155e4bb4c63287fa063de
rashid <rshaikh@equalexperts.com>
2016-06-17 13:29:03 +0530

    Add Money abstraction

---
+++ src/main/java/Money.java
@@ -0,0 +1,41 @@
+/**
+ * Created by rashid on 17/6/16.
+ */
+public class Money {
+    private int amount;
+    private String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    public String currency(){
+        return currency;
+    }
+    public Money times(int multiplier){
+        return new Money(amount * multiplier, currency);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Money money = (Money) o;
+        return amount == money.amount && currency.equals(money.currency) ;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+}
